0369752ad47a1662655e2c65b328a0ba5dd3a934
chenyaofo <chenyaofo@gmail.com>
2018-08-04 22:55:00 +0800

    major functions complete.

---
+++ mio/utils.py
@@ -0,0 +1,22 @@
+import struct
+
+from io import BytesIO
+from PIL import Image
+
+
+class Serializer(object):
+    def __init__(self, format_):
+        self.format_ = struct.Struct(format_)
+
+    def read(self, buffer):
+        return list(self.format_.iter_unpack(buffer))
+
+    def write(self, f, items):
+        for item in items:
+            f.write(self.format_.pack(*item))
+
+
+def bytes2image(b):
+    b = BytesIO(b)
+    img = Image.open(b)
+    return img
